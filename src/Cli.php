a9a308aaa6ddbf4af94156e164da44cfa7ba4cae
Svetlana <radalana415@gmail.com>
2023-09-04 18:45:18 +0200

    fix linter src/Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Brain-Games CLI
+ * greets user
+ * php version 8.1.2
+ 
+ * @category CLI
+ * @package  Code
+ * @author   Sveta <radalana415@gmail.com>
+ * @link     https://github.com/radalana/php-project-45.git
+ */
 namespace Brain\Games\Cli;
 require_once __DIR__ . '../../vendor/autoload.php';
 
@@ -7,9 +17,14 @@ require_once __DIR__ . '../../vendor/autoload.php';
 use function cli\line;
 use function cli\prompt;
 
+/**
+ * Asks user's name und display a greeting with the user's name
+ 
+ * @return void
+ */
 function greet()
 {
-line('Welcome to the Brain Game!');
-$name =  prompt('May I have your name?');
-line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name =  prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
