a6b150fc2d460fd980504784f634d22a9ea02e79
Svetlana <radalana415@gmail.com>
2023-09-04 16:06:08 +0200

    update src/

---
+++ src/Cli.php
@@ -0,0 +1,15 @@
+<?php
+namespace Brain\Games\Cli;
+require_once __DIR__ . '../../vendor/autoload.php';
+
+
+
+use function cli\line;
+use function cli\prompt;
+
+function greet()
+{
+line('Welcome to the Brain Game!');
+$name =  prompt('May I have your name?');
+line("Hello, %s!", $name);
+}
