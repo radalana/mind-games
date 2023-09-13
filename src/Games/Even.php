85db57b1b8a6b92b852d7813769e4beab936b56a
Svetlana <radalana415@gmail.com>
2023-09-13 16:50:49 +0200

    Update game brain-even
    изменена структура теперь приветвие вынесено и вся логика игры в scr/Games/Even.php

---
+++ src/Games/Even.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Brain\Games\Even;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+use function Brain\Games\Engine\greet;
+use function cli\line;
+use function cli\prompt;
+
+
+function isEven($number) : string
+{
+    if ($number % 2 === 0){
+        return 'yes';
+    }
+
+    return 'no'; 
+}
+
+function playEven(){
+    $name = greet();
+    line('Answer "yes" if the number is even, otherwise answer "no".');
+    
+    $numberOfRounds = 3;
+    $currentRound = 0;
+
+for ($currentRound; $currentRound < $numberOfRounds; $currentRound++)
+{
+    $randomNumber = rand();
+    line('Question: %d', $randomNumber);
+    $userAnswer = prompt('Your answer');
+    $correctAnswer = isEven($randomNumber);
+
+    if ($userAnswer != $correctAnswer)
+    {
+        line("'%s' is wrong answer ;(. Correct anwer was '%s'.", $userAnswer, $correctAnswer);
+        line("Let's try again, %s!", $name);
+        break;
+    }
+
+    line('Correct!');
+}
+
+if ($currentRound === $numberOfRounds){
+    line('Congratulations, %s!', $name);
+}
+}
