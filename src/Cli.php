e8388237423a36cf714b1b5310cf7d5604c2add3
Svetlana <radalana415@gmail.com>
2023-09-14 13:30:22 +0200

    Refactor Cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -8,7 +8,7 @@ use function cli\line;
 use function cli\prompt;
 
 function greet(){
-    line('Welcome to the Brain Game!');
+    line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
