54e19248904cdc970de7b68185e3b329ccb2f6d1
Svetlana <radalana415@gmail.com>
2023-09-29 20:34:23 +0200

    Update src/Games/Gcd.php
    общая логика игри  на нахождение нод изменена так общая часть вынесена в движок src/Engine.php

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -2,50 +2,39 @@
 
 namespace Code\Games\Gcd;
 
-use function Code\Engine\greet;
+use function Code\Engine\playGame;
 use function cli\line;
-use function cli\prompt;
-
-function calculateGreatestCommonDeviser(int $firstNumber, int $secondNumber): int
-{
-    if ($firstNumber === 0 || $secondNumber === 0) {
-        return $firstNumber + $secondNumber;
-    }
-    while ($firstNumber != $secondNumber) {
-        if ($firstNumber > $secondNumber) {
-            $firstNumber = $firstNumber - $secondNumber;
-        } else {
-            $secondNumber = $secondNumber - $firstNumber;
-        }
-    }
-
-    return $firstNumber;
-}
 
 function findGreatestCommonDeviser()
 {
-    $name = greet();
-    line('Find the greatest common divisor of given numbers.');
-
-    $numberOfRounds = 3;
-    $currentRound = 0;
-
-    for ($currentRound; $currentRound < $numberOfRounds; $currentRound++) {
+    $printTask = function () {
+        line('Find the greatest common divisor of given numbers.');
+    };
+    
+    $generateQuestion = function () {
         $firstNumber = rand(0, 50);
         $secondNumber = rand(0, 50);
-        line('Question: %d %d', $firstNumber, $secondNumber);
+        return "{$firstNumber} {$secondNumber}";
+    };
 
-        $userAnswer = prompt('Your answer');
-        $correctAnswer = calculateGreatestCommonDeviser($firstNumber, $secondNumber);
+    $calculateGreatestCommonDeviser = function (string $stringTask) {
+        $arrayNumbers = explode(" ", $stringTask);
+        $firstNumber = $arrayNumbers[0];
+        $secondNumber = $arrayNumbers[1];
 
-        if ($userAnswer != $correctAnswer) {
-            line("'%d' is wrong answer ;(. Correct anwer was '%d'.", $userAnswer, $correctAnswer);
-            line("Let's try again, %s!", $name);
-            break;
+        if ($firstNumber === 0 || $secondNumber === 0) {
+            return $firstNumber + $secondNumber;
         }
-    }
+        while ($firstNumber != $secondNumber) {
+            if ($firstNumber > $secondNumber) {
+                $firstNumber = $firstNumber - $secondNumber;
+            } else {
+                $secondNumber = $secondNumber - $firstNumber;
+            }
+        }
 
-    if ($currentRound === $numberOfRounds) {
-        line('Congratulations, %s!', $name);
-    }
+        return $firstNumber;
+    };
+
+    playGame($generateQuestion, $calculateGreatestCommonDeviser, $printTask);
 }
