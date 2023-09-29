58c30f1e0aa195ad4eda9ef63fa3204927395a21
Svetlana <radalana415@gmail.com>
2023-09-29 20:22:39 +0200

    Update src/Games/Calc.php
    Общая логика изменена и вынесена в движок

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -2,13 +2,34 @@
 
 namespace Code\Games\Calc;
 
-use function Code\Engine\greet;
+use function Code\Engine\playGame;
 use function cli\line;
-use function cli\prompt;
 
-function calculateExpression(int $firstNumber, int $secondNumber, string $operation)
+function playCalculation()
 {
-    switch ($operation) {
+    $printTask = function () {
+        line('What is the result of the expression?');
+    };
+
+
+    $genereteExpression = function () {
+        $firstNumber  = rand(0, 100);
+        $secondNumber = rand(0, 100);
+
+        $operations = ['+', '-', '*'];
+        $operation = $operations[rand(0, 2)];
+
+        return "{$firstNumber} {$operation} {$secondNumber}";
+    };
+
+    $calculateExpression = function (string $stringExpression) {
+        $arrayExpression = explode(" ", $stringExpression);
+        $firstNumber = $arrayExpression[0];
+        $secondNumber = $arrayExpression[2];
+
+        $operation = $arrayExpression[1];
+
+        switch ($operation) {
         case '+':
             return $firstNumber + $secondNumber;
         case '-':
@@ -16,40 +37,11 @@ function calculateExpression(int $firstNumber, int $secondNumber, string $operat
         case '*':
             return $firstNumber * $secondNumber;
         default:
-            return null;
-    }
-}
-
-
-
-function playCalculation()
-{
-    $name = greet();
-    print_r('What is the result of the expression?');
-
-    $numberOfRounds = 3;
-    $currentRound = 0;
-
-    $operations = ['+', '-', '*'];
-    for ($currentRound; $currentRound < $numberOfRounds; $currentRound++) {
-        $firstNumber  = rand(0, 100);
-        $secondNumber = rand(0, 100);
-        $operation = $operations[rand(0, 2)];
-        line('Question: %d %s %d', $firstNumber, $operation, $secondNumber);
-
-        $userAnswer = prompt('Your answer');
-        $correctAnswer = calculateExpression($firstNumber, $secondNumber, $operation);
-
-        if ($userAnswer != $correctAnswer) {
-            line("'%d' is wrong answer ;(. Correct anwer was '%d'.", $userAnswer, $correctAnswer);
-            line("Let's try again, %s!", $name);
-            break;
+            return throw new \Exception('Error: not valid operation!');
         }
+    };
 
-        line('Correct!');
-    }
-
-    if ($currentRound === $numberOfRounds) {
-        line('Congratulations, %s!', $name);
-    }
+    
+    
+    playGame($genereteExpression, $calculateExpression, $printTask);
 }
