93fd81157ac938f31600e24f361340b61365ceae
Svetlana <radalana415@gmail.com>
2023-10-04 17:12:46 +0200

    Update src/Games/Prime.php
    замена анон функций на именные

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -3,36 +3,38 @@
 namespace Code\Games\Prime;
 
 use function Code\Engine\playGame;
-use function cli\line;
+
+function generateNumber()
+{
+    return rand(1, 3571);
+}
+
+function isPrime(int $number)
+{
+    if (($number === 1) || ($number === 2)) {
+        return 'no';
+    }
+
+
+    if (($number % 2 === 0) || ($number % 3 === 0)) {
+        return 'no';
+    }
+
+    $border = round(sqrt($number));
+
+    $potentialDivisers = 5;
+
+    for ($potentialDivisers; $potentialDivisers <= $border; $potentialDivisers += 2) {
+        if ($number % $potentialDivisers == 0) {
+            return 'no';
+        }
+    }
+
+    return 'yes';
+}
 
 function findPrime()
 {
     $task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-    $generateNumber = function () {
-        return rand(1, 3571);
-    };
-    $isPrime = function (int $number) {
-        if (($number === 1) || ($number === 2)) {
-            return 'no';
-        }
-
-
-        if (($number % 2 === 0) || ($number % 3 === 0)) {
-            return 'no';
-        }
-
-        $border = round(sqrt($number));
-
-        $potentialDivisers = 5;
-
-        for ($potentialDivisers; $potentialDivisers <= $border; $potentialDivisers += 2) {
-            if ($number % $potentialDivisers == 0) {
-                return 'no';
-            }
-        }
-
-        return 'yes';
-    };
-
-    playGame($generateNumber, $isPrime, $task);
+    playGame('Code\Games\Prime\generateNumber', 'Code\Games\Prime\isPrime', $task);
 }
