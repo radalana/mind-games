fad782a70854d9dda28ebf32745027c6659ce840
Svetlana <radalana415@gmail.com>
2023-10-04 17:06:21 +0200

    Update src/Games/Calc.php
    замена анонимных функций на именные

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -3,40 +3,40 @@
 namespace Code\Games\Calc;
 
 use function Code\Engine\playGame;
-use function cli\line;
+
+function genereteExpression()
+{
+    $firstNumber  = rand(0, 100);
+    $secondNumber = rand(0, 100);
+
+    $operations = ['+', '-', '*'];
+    $operation = $operations[rand(0, 2)];
+
+    return "{$firstNumber} {$operation} {$secondNumber}";
+}
+
+function calculateExpression(string $stringExpression)
+{
+    $arrayExpression = explode(" ", $stringExpression);
+    $firstNumber = (int) $arrayExpression[0];
+    $secondNumber = (int) $arrayExpression[2];
+
+    $operation = $arrayExpression[1];
+
+    switch ($operation) {
+        case '+':
+            return $firstNumber + $secondNumber;
+        case '-':
+            return $firstNumber - $secondNumber;
+        case '*':
+            return $firstNumber * $secondNumber;
+        default:
+            return throw new \Exception('Error: not valid operation!');
+    }
+}
 
 function playCalculation()
 {
     $task = 'What is the result of the expression?';
-
-    $genereteExpression = function () {
-        $firstNumber  = rand(0, 100);
-        $secondNumber = rand(0, 100);
-
-        $operations = ['+', '-', '*'];
-        $operation = $operations[rand(0, 2)];
-
-        return "{$firstNumber} {$operation} {$secondNumber}";
-    };
-
-    $calculateExpression = function (string $stringExpression) {
-        $arrayExpression = explode(" ", $stringExpression);
-        $firstNumber = (int) $arrayExpression[0];
-        $secondNumber = (int) $arrayExpression[2];
-
-        $operation = $arrayExpression[1];
-
-        switch ($operation) {
-            case '+':
-                return $firstNumber + $secondNumber;
-            case '-':
-                return $firstNumber - $secondNumber;
-            case '*':
-                return $firstNumber * $secondNumber;
-            default:
-                return throw new \Exception('Error: not valid operation!');
-        }
-    };
-
-    playGame($genereteExpression, $calculateExpression, $task);
+    playGame('Code\Games\Calc\genereteExpression', 'Code\Games\Calc\calculateExpression', $task);
 }
