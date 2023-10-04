031bd9a3e1517e866d1cba0db2e2428840084f18
Svetlana <radalana415@gmail.com>
2023-10-04 17:09:53 +0200

    Update src/Games/Even.php
    замена анонимных функций на именные

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -3,21 +3,22 @@
 namespace Code\Games\Even;
 
 use function Code\Engine\playGame;
-use function cli\line;
+
+function isEven(int $number)
+{
+    if ($number % 2 === 0) {
+        return 'yes';
+    }
+    return 'no';
+}
+
+function generateNumber()
+{
+    return rand();
+}
 
 function playEven()
 {
-    $isEven = function (int $number) {
-        if ($number % 2 === 0) {
-            return 'yes';
-        }
-        return 'no';
-    };
-    $generateNumber = function () {
-        return rand();
-    };
-    
     $task = 'Answer "yes" if the number is even, otherwise answer "no".';
-
-    playGame($generateNumber, $isEven, $task);
+    playGame('Code\Games\Even\generateNumber', 'Code\Games\Even\isEven', $task);
 }
