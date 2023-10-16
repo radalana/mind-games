ce8d6c9efb13fdb6ac911d80847669aa72f9ce1f
Svetlana <radalana415@gmail.com>
2023-10-16 21:31:37 +0200

    Fix src/Games/Progression.php
    declare $progression

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -9,8 +9,8 @@ const DESCRIPTION = 'What number is missing in the progression?';
 function generateProgression(int $lengthOfProgression)
 {
     $firstNumber = rand(-10, 10);
-
     $diff = rand(-10, 10);
+    $progression = [];
 
     for ($i = 0; $i < $lengthOfProgression; $i++) {
         $progression[$i] = $firstNumber + ($i * $diff);
